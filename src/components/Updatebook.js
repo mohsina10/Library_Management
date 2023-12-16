a98c35215e9b6fd68b3d9aa8235e5cf05ca0cd2d
Mohsina Mulani <mohsinamulani10@gmail.com>
2023-12-16 11:22:32 +0530

    serching in view book added

--- src/components/Updatebook.js
+++ src/components/Updatebook.js
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from "react";
-import {  useNavigate} from "react-router-dom";
- import axios from "axios";
- function Updatebook() { 
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+function Updatebook() {
   const navigate = useNavigate();
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [updatedData, setUpdatedData] = useState({
-    bookname: '',
-    author: '', 
-    date:'',
-   
-  }); 
+    bookname: "",
+    author: "",
+    date: "",
+  });
   const handleUpdate = async () => {
     try {
-      console.log(updatedData);
-      const response = await axios.put(`http://localhost:5000/api/update/${name}`, updatedData);
-      console.log(response.data.message); 
-      if(response) 
-      {
-        alert("Data Updated Succesfully :)"); 
+      // console.log(updatedData);
+      const response = await axios.put(
+        `http://localhost:5000/api/update/${name}`,
+        updatedData
+      );
+      console.log(response.data.message);
+      if (response) {
+        alert("Data Updated Succesfully :)");
         navigate("../");
       }
     } catch (error) {
-      console.error('Error updating item:', error);
-  
+      console.error("Error updating item:", error);
     }
   };
 
@@ -43,9 +43,9 @@ import {  useNavigate} from "react-router-dom";
             </li>
           </ul>
         </nav>
-      </header>  
-      <div className='doc'>
-      <h2 >Update book !!</h2> 
+      </header>
+      <div className="doc">
+        <h2>Update book !!</h2>
       </div>
       {/* <div className="container mt-5">
         <form>
@@ -93,39 +93,76 @@ import {  useNavigate} from "react-router-dom";
           </button>
         </form>
   </div>  */}
-        <div className="container mt-5">
+      <div className="container mt-5">
         <form>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">
               Book Name
             </label>
-       <input type="text"  className="form-control" placeholder="bookname" value={name} onChange={(e) => setName(e.target.value)} />
-       </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="bookname"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
           <div className="mb-3">
             <div className="form-group">
-              <label className="form-label" htmlFor="name">New Book Name</label>
-      <input type="text" className="form-control" placeholder="new bookname" value={updatedData.bookname} onChange={(e) => setUpdatedData({ ...updatedData, bookname: e.target.value })} /> 
-      </div> 
-      </div>
-            <div className="mb-3">
+              <label className="form-label" htmlFor="name">
+                New Book Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="new bookname"
+                value={updatedData.bookname}
+                onChange={(e) =>
+                  setUpdatedData({ ...updatedData, bookname: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className="mb-3">
             <div className="form-group">
-              <label className="form-label" htmlFor="name">Author</label>
-      <input type="text" className="form-control"  placeholder="author" value={updatedData.author} onChange={(e) => setUpdatedData({ ...updatedData, author: e.target.value })} /> 
-      </div>
+              <label className="form-label" htmlFor="name">
+                Author
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="author"
+                value={updatedData.author}
+                onChange={(e) =>
+                  setUpdatedData({ ...updatedData, author: e.target.value })
+                }
+              />
+            </div>
           </div>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">
               Date
             </label>
-      <input type="text" className="form-control" placeholder="date" value={updatedData.date} onChange={(e) => setUpdatedData({ ...updatedData, date: e.target.value })} /> 
-      </div>
-          <button type="submit" className="btn btn-success" onClick={handleUpdate}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="date"
+              value={updatedData.date}
+              onChange={(e) =>
+                setUpdatedData({ ...updatedData, date: e.target.value })
+              }
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-success"
+            onClick={handleUpdate}
+          >
             Submit
           </button>
         </form>
-      </div> 
-     
-     </div>
-  )
+      </div>
+    </div>
+  );
 }
 export default Updatebook;
--- src/components/Viewbook.js
+++ src/components/Viewbook.js
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from "react";
 // import axios from "axios";
 function Viewbook() {
-  const [data, setData] = useState([]);  
+  const [data, setData] = useState([]);
+  const [mydata, setMydata] = useState([]);
+  const [name, setName] = useState("");
+  const [myd, setMyd] = useState(0);
+  const handleUpdate = async () => {
+    setMyd(1);
+    const foundData = data.filter(item => item.bookname === name);
+    if (foundData) {
+      setMydata(foundData);
+    }
+   console.log(foundData);
+
+  }; 
+  console.log(name); 
+  console.log(myd);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/data');
+        const response = await fetch("http://localhost:5000/api/data");
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -20,7 +34,7 @@ function Viewbook() {
   }, []);
 
   return (
-    <div> 
+    <div>
       <header>
         <nav className="navbar">
           <div className="logo">Library</div>
@@ -37,35 +51,53 @@ function Viewbook() {
           </ul>
         </nav>
       </header>
-      <div className='doc'>
-      <h1>Books List</h1> 
+      <div className="doc">
+        <h1>Books List</h1>
       </div> 
+      <div className="search">
+      <input
+        type="text"
+        placeholder="Enter Bookname"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type="submit" className="btn btn-success" onClick={handleUpdate}>
+        Search
+      </button> 
+      </div>
       <table className="table">
-      <thead>
-        <tr >
-        <th scope="col">Sr No.</th>
-      <th scope="col">Book Name</th>
-      <th scope="col">Author</th>
-      <th scope="col">Date</th>
-        </tr>
-      </thead>
-      <tbody>
-      {data.map((item,index) => (
-          <tr key={item._id} scope="row" > 
-           <td> {index+1}</td>
-           <td> 
-          {item.bookname }
-          </td>  
-          <td> 
-          {item.author }
-         </td>   
-          <td> 
-          {item.date }
-          </td>
-          </tr> 
-          ))}
-      </tbody>
-    </table>
+        <thead>
+          <tr>
+            <th scope="col">Sr No.</th>
+            <th scope="col">Book Name</th>
+            <th scope="col">Author</th>
+            <th scope="col">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          { 
+           myd === 1  ? (
+            mydata.map((item, index) => (
+            <tr key={item._id} scope="row">
+              <td>{index+1}</td>
+              <td>{item.bookname}</td>
+              <td>{item.author}</td>
+              <td>{item.date}</td>
+            </tr>
+            ))
+          ) : (
+            data.map((item, index) => (
+              <tr key={item._id} scope="row">
+                <td>{index + 1}</td>
+                <td>{item.bookname}</td>
+                <td>{item.author}</td>
+                <td>{item.date}</td>
+              </tr>
+            ))
+          )}
+            
+        </tbody>
+      </table>
     </div>
   );
 }
